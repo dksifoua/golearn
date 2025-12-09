2bffc4cf887c7b6bd265c9d2ac0bcbb40e4264f6
dksifoua <dimitri.sifoua@gmail.com>
2025-12-09 16:32:35 -0500

    GoLang Context

---
+++ pkg/context/server.go
@@ -0,0 +1,24 @@
+package context
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		data, err := store.Fetch(request.Context())
+		if err != nil {
+			return
+		}
+
+		if _, err := fmt.Fprint(writer, data); err != nil {
+			return
+		}
+	}
+}
